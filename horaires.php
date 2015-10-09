1ea81a4c4933ce7b49dd9327e7cfd553f709af51
greeg4 <mathieu405@hotmail.com>
2015-10-09 10:07:16 +0200

    page horaires

---
+++ horaires.php
@@ -0,0 +1,9 @@
+<h2>Heures d'ouverture du magasin</h2>
+<h3>Acheter online</h3>
+    <p>Le magasin en ligne est ouvert 7/24</p>    
+<h3>Retirer ses achats</h3>
+    <p>Les entrepôts sont ouverts aux jours et heures suivants :</p>
+    <ul>
+        <li>Mardi : 10h - 18h</li>
+        <li>Vendredi : 10h - 19h</li>
+    </ul>
